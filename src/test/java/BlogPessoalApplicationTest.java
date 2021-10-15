502a0ba7994cca8e10cce7b2fd615dde59d251ac
EstherMelo <emelo3172@gmail.com>
2021-10-14 23:32:32 -0300

    new commit

---
+++ src/test/java/BlogPessoalApplicationTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BlogPessoalApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
